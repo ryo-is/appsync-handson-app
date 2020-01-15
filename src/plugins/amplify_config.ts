ed4db78c6b7dbd78982d763a16b6895e00f2bb77
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2020-01-15 16:07:11 +0900

    modfied

--- src/plugins/amplify_config.ts
+++ src/plugins/amplify_config.ts
@@ -1,7 +1,6 @@
 export const amplifyConfig: { [k: string]: any } = {
-  aws_appsync_graphqlEndpoint:
-    "https://lrtdgwcykzde5mzmx4tdrbzvdq.appsync-api.ap-northeast-1.amazonaws.com/graphql",
-  aws_appsync_region: "ap-northeast-1",
-  aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: "da2-pt4743b3q5gidkssif7s5gwnr4"
+  aws_appsync_graphqlEndpoint: "",
+  aws_appsync_region: "",
+  aws_appsync_authenticationType: "",
+  aws_appsync_apiKey: ""
 }
